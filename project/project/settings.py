675268517b6d01e464f4b7658bd6c4102c08b057
SanderAMC <sanderamc@gmail.com>
2022-12-29 12:07:43 +0300

    Site, v.1

--- project/project/settings.py
+++ project/project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages',
 ]
 
 SITE_ID = 1
@@ -124,7 +125,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
